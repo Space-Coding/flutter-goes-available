09dc374d299ca3f1d7050c36eb2ea20b2ed6c2f6
Shohijahon Ikromov <trogrammer@gmail.com>
2022-07-09 01:53:22 +0500

    oraliq commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(FirstApp());
 
-class FirstApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello'));
+class FirstApp extends StatelessWidget {
+
+  void myFunctionForOnPress(){
+    print("Button Pressed");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('Hello World'),
+            ElevatedButton(
+              onPressed: myFunctionForOnPress,
+              child: Text('Press me!'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
